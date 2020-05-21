904a872b04e4df51e2f4de78d7a94d455370eef6
Dhaval Shah <d.shah@tum.de>
2020-05-21 17:44:25 +0200

    Done exercise 3

--- Exercise03/evaluation.py
+++ Exercise03/evaluation.py
@@ -2,10 +2,15 @@
 
 import torch
 
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
 def check_accuracy(loader, model, device):   
     num_correct = 0
     num_samples = 0
     ## TODO: Set model to evaluation mode.
+    model.eval()
+    softmax = torch.nn.Softmax(dim=1)
 
     with torch.no_grad():
         for inputs, labels in loader:
@@ -13,11 +18,13 @@ def check_accuracy(loader, model, device):
             labels = labels.to(device=device)
             
             ## TODO: Pass inputs through the model. 
-            
+            outputs = model(inputs)
+#             print(outputs)
             
             ## TODO: Collect the prediction classes according to probability values. 
-            scores =  
-            _, preds = 
+            scores =  softmax(outputs)
+#             print(scores)
+            _, preds = torch.max(scores, 1)
             
             num_correct += (preds == labels).sum()
             num_samples += preds.size(0)
@@ -29,19 +36,19 @@ def check_accuracy_perclass(loader, model, device):
     class_correct = list(0. for i in range(10))
     class_total = list(0. for i in range(10))
     ## TODO: Set model to evaluation mode.
-
+    model.eval()
 
     with torch.no_grad():
-        for inputs, labels in testloader:
+        for inputs, labels in loader:
 
             inputs = inputs.to(device=device)  # move to device, e.g. GPU
             labels = labels.to(device=device) 
 
             ## TODO: Pass inputs through the model. 
-            
+            outputs = model(inputs)
             
             ## TODO: Collect the prediction classes according to probability values. 
-            
+            _, preds = torch.max(outputs, 1)
             
             c = (preds == labels).squeeze()
             for i in range(4):
--- Exercise03/graph.py
+++ Exercise03/graph.py
@@ -6,16 +6,39 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self, NUM_CLASSES=10):
         super(Net, self).__init__()
-
+        self.num_classes = NUM_CLASSES
 
         # ***** TODO: Define the network layers *****
-
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5,
+                               stride=1, padding=3)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3,
+                               stride=1, padding=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3,
+                               stride=1, padding=2)
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3,
+                               stride=1, padding=1)
         
-        
+        self.pool = nn.MaxPool2d(kernel_size=3, stride=2)
 
+        self.fc1 = nn.Linear(in_features=2048, out_features=384)
+        self.fc2 = nn.Linear(in_features=384, out_features=self.num_classes)
+
+        self.relu = nn.ReLU(inplace=True)
+
+#         self.softmax = nn.Softmax(dim=1)
+    
     def forward(self, x):
         
-        # ***** TODO: Implement the forward pass *****
+         # ***** TODO: Implement the forward pass *****
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+        x = self.relu(self.conv4(x))
+
+
+        x = self.relu(self.fc1(x.view(-1, 2048)))
+        x = self.fc2(x)
+
+#         x = self.softmax(x)
         
-        
-        return logits
+        return x
