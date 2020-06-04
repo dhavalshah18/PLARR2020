c7538e6578a37e80f3b13d353ea8b6db15469103
Mahdi Saleh <m.saleh@tum.de>
2020-06-04 21:10:12 +0200

    Added Exercise 06

---
+++ Exercise06/src/Exercise06_1/main.py
@@ -0,0 +1,15 @@
+import open3d as o3d
+
+if __name__ == '__main__':
+
+    # Load .pcd file to open3d.geometry.PointCloud
+    filename_pcd = '../../data/bun0.pcd'
+
+    # Visualize the point clouds using draw_geometries
+    
+
+    # Compute normals of the point clouds
+    
+
+    # Visualize the point clouds with computed normals
+    
